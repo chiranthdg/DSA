74aa4d6f9e9c2334621b8887a4a432b62f7d436b
chiranthdg <55840829+chiranthdg@users.noreply.github.com>
2021-10-19 16:49:30 +0530

    Create comment execution.java

---
+++ comment execution.java
@@ -0,0 +1,6 @@
+public class Test{  
+    public static void main(String[] args) {  
+        //the below comment will be executed  
+// \u000d System.out.println("Java comment is executed!!");  
+    }  
+}  
