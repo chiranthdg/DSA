0b4d2dedeb54acb079300582ff82e0d24341f900
chiranthdg <55840829+chiranthdg@users.noreply.github.com>
2021-11-04 20:57:26 +0530

    Create pystar.py

---
+++ pystar.py
@@ -0,0 +1,14 @@
+import turtle
+ws = turtle.Screen()
+ 
+# initializing a turtle instance
+geekyTurtle = turtle.Turtle()
+ 
+# executing loop 5 times for a star
+for i in range(5):
+ 
+        # moving turtle 100 units forward
+        geekyTurtle.forward(100)
+ 
+        # rotating turtle 144 degree right
+        geekyTurtle.right(144)
